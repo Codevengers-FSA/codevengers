5bccaf232317da13130577b60aedb0b7a086353b
Umaid Ismail <umaidismail@umaids-air.mynetworksettings.com>
2024-12-03 10:50:06 -0800

    add phase 3

---
+++ prisma/phases/phase3.js
@@ -0,0 +1,80 @@
+const phase3 = {
+  movie: {
+    ratings: "90% on Rotten Tomatoes",
+    watchlists: [],
+    title: "Captain America: Civil War",
+    summary: `Political pressure mounts to install a system of accountability when the actions of the Avengers lead to collateral damage. The new status quo deeply divides members of the team. Captain America (Chris Evans) believes superheroes should remain free to defend humanity without government interference. Iron Man (Robert Downey Jr.) sharply disagrees and supports oversight. As the debate escalates into an all-out feud, Black Widow (Scarlett Johansson) and Hawkeye (Jeremy Renner) must pick a side.
+Content expanded.`,
+    image: "https://resizing.flixster.com/JbXw8Y5fwrZxzLKUwoFoMGY1JxE=/206x305/v2/https://resizing.flixster.com/1nBX_V-Cyl3s_VE5LBBZR0sXvvo=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzL2M3NGMwNGE0LTE2MWMtNDIzYS1hMTQyLWY0ZWM0MGU4ZGIxNC53ZWJw",
+  },
+  movie2: {
+    ratings: "89% on Rotten Tomatoes",
+    watchlists: [],
+    title: "Doctor Strange",
+    summary: `Dr. Stephen Strange's (Benedict Cumberbatch) life changes after a car accident robs him of the use of his hands. When traditional medicine fails him, he looks for healing, and hope, in a mysterious enclave. He quickly learns that the enclave is at the front line of a battle against unseen dark forces bent on destroying reality. Before long, Strange is forced to choose between his life of fortune and status or leave it all behind to defend the world as the most powerful sorcerer in existence.`,
+    image: "https://resizing.flixster.com/yMZoUf8f60ULi8Yg3bajRou6lUs=/206x305/v2/https://resizing.flixster.com/2fRqqQGBPzHKkj-Z4S4UU4I2JXA=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzgzOGIzMTBlLTdiZWUtNDMyNy1hZjU0LTc5NzcyOWM3OWY3ZS53ZWJw",
+  },
+  movie3: {
+    ratings: "85% on Rotten Tomatoes",
+    watchlists: [],
+    title: `Guardians of the Galaxy Vol.2`,
+    summary: `Peter Quill and his fellow Guardians are hired by a powerful alien race, the Sovereign, to protect their precious batteries from invaders. When it is discovered that Rocket has stolen the items they were sent to guard, the Sovereign dispatch their armada to search for vengeance. As the Guardians try to escape, the mystery of Peter's parentage is revealed.`,
+    image: `https://resizing.flixster.com/MUpU9G5bejwhffALcOhV_ORj7sA=/206x305/v2/https://resizing.flixster.com/SY3NRybei-FtvHL-p2LrtZb0elY=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzI0ZDRiZmQ0LTI0ZjUtNDg5MC05NDUxLTdhMzdhMmU3NTMzOC53ZWJw`,
+  },
+  movie4: {
+    ratings: "92% on Rotten Tomatoes",
+    watchlists: [],
+    title: `Spider-Man: Homecoming`,
+    summary: `Thrilled by his experience with the Avengers, young Peter Parker returns home to live with his Aunt May. Under the watchful eye of mentor Tony Stark, Parker starts to embrace his newfound identity as Spider-Man. He also tries to return to his normal daily routine -- distracted by thoughts of proving himself to be more than just a friendly neighborhood superhero. Peter must soon put his powers to the test when the evil Vulture emerges to threaten everything that he holds dear.`,
+    image: `https://resizing.flixster.com/FvwtIbm77-sXnBU1gZpL8OsDHVg=/206x305/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p12798844_p_v8_ao.jpg`,
+  },
+  movie5: {
+    ratings: "93% on Rotten Tomatoes",
+    watchlists: [],
+    title: `Thor: Ragnorok`,
+    summary: `Imprisoned on the other side of the universe, the mighty Thor finds himself in a deadly gladiatorial contest that pits him against the Hulk, his former ally and fellow Avenger. Thor's quest for survival leads him in a race against time to prevent the all-powerful Hela from destroying his home world and the Asgardian civilization.`,
+    image: `https://resizing.flixster.com/sgOB_IkAGoqHjw9CYA1hLOu5RNU=/206x305/v2/https://resizing.flixster.com/cfMzccfl2JCRfmwdK2cCVqBIAlc=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzI0NDdkYmJhLTY2MmEtNDY5My04MzMzLTFjMzZhYmM4MDkzZC53ZWJw`,
+  },
+  movie6: {
+    ratings: `96% on Rotten Tomatoes`,
+    watchlists: [],
+    title: `Black Panther`,
+    summary: `After the death of his father, T'Challa returns home to the African nation of Wakanda to take his rightful place as king. When a powerful enemy suddenly reappears, T'Challa's mettle as king -- and as Black Panther -- gets tested when he's drawn into a conflict that puts the fate of Wakanda and the entire world at risk. Faced with treachery and danger, the young king must rally his allies and release the full power of Black Panther to defeat his foes and secure the safety of his people.`,
+    image: `https://resizing.flixster.com/z27vZVXCRmpvETmdJQEW9Il7Z2Y=/206x305/v2/https://resizing.flixster.com/KBlur3LaA-y1U1yt6_Y2uO25ozA=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzMxOGI1YzBhLWMyMjEtNGUxMS1iM2Q0LWQ4OGMyYzQyZjQyYS53ZWJw`,
+  },
+  movie7: {
+    ratings: `85% on Rotten Tomatoes`,
+    watchlists: [],
+    title: "Avengers: Infinity War",
+    summary: `Iron Man, Thor, the Hulk and the rest of the Avengers unite to battle their most powerful enemy yet -- the evil Thanos. On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality. The fate of the planet and existence itself has never been more uncertain as everything the Avengers have fought for has led up to this moment.`,
+    image: `https://resizing.flixster.com/xC06wd4ol1CFog1PZ34PRcr_akg=/206x305/v2/https://resizing.flixster.com/CXOXbOpLNL1NNkXTQu-4Rgvcszs=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzM0NGRkMDM2LWVjNDQtNGZlMC04NGM3LWZkMzQ2Njg1OTUyNi53ZWJw`,
+  },
+  movie8: {
+    ratings: `87% on Rotten Tomatoes`,
+    watchlists: [],
+    title: `Ant-Man and The Wasp`,
+    summary: `Scott Lang is grappling with the consequences of his choices as both a superhero and a father. Approached by Hope van Dyne and Dr. Hank Pym, Lang must once again don the Ant-Man suit and fight alongside the Wasp. The urgent mission soon leads to secret revelations from the past as the dynamic duo finds itself in an epic battle against a powerful new enemy.`,
+    image: `https://resizing.flixster.com/u4_quCnzNTjmZlPxD9EkKeLnZAA=/206x305/v2/https://resizing.flixster.com/OQT-Iar3VPUPi_xSo6EWdx_RS38=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzQyNTBjMzY2LWJiMTItNDc3YS04ZDFiLWFiYTcxNzM4NThlZC53ZWJw`,
+  },
+  movie9: {
+    ratings: `79% on Rotten Tomatoes`,
+    watchlists: [],
+    title: `Captain Marvel`,
+    summary: `Captain Marvel is an extraterrestrial Kree warrior who finds herself caught in the middle of an intergalactic battle between her people and the Skrulls. Living on Earth in 1995, she keeps having recurring memories of another life as U.S. Air Force pilot Carol Danvers. With help from Nick Fury, Captain Marvel tries to uncover the secrets of her past while harnessing her special superpowers to end the war with the evil Skrulls.`,
+    image: `https://resizing.flixster.com/G_HcAEIXYFKFW2_tkLw3ocnx1ZQ=/206x305/v2/https://resizing.flixster.com/IATaLQOAfVzJ-wfnzriXuACjmgU=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzU5ZjVjZTkzLTkwYjEtNDI5My04ZTY0LWMwNGMzM2M0MmYwOS53ZWJw`,
+  },
+  movie10: {
+    ratings: `94% on Rotten Tomatoes`,
+    watchlists: [],
+    title: `Avengers Endgame`,
+    summary: `The fourth installment in the Avengers saga is the culmination of 22 interconnected Marvel films and the climax of a journey. The world's heroes finally understand just how fragile reality is, and the sacrifices that must be made to uphold it, in a story of friendship, teamwork and setting aside differences to overcome an impossible obstacle.`,
+    image: `https://resizing.flixster.com/2yTtbYaljlzWgEhOCUTrH55jjfM=/206x305/v2/https://resizing.flixster.com/fC7nU6iTRQk02tS0SDS1ylx-G34=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzL2QxZjE5ZDgzLTRiY2MtNDFkYS04NWQ4LTRkYzc1ZTAwNWE2NC53ZWJw`,
+  },
+  movie11: {
+    ratings: `91% on Rotten Tomatoes`,
+    watchlists: [],
+    title: `Spider-Man: Far From Home`,
+    summary: `Peter Parker's relaxing European vacation takes an unexpected turn when Nick Fury shows up in his hotel room to recruit him for a mission. The world is in danger as four massive elemental creatures -- each representing Earth, air, water and fire -- emerge from a hole torn in the universe. Parker soon finds himself donning the Spider-Man suit to help Fury and fellow superhero Mysterio stop the evil entities from wreaking havoc across the continent.`,
+    image: `https://resizing.flixster.com/vMxRk7RVIUWAlLkKFWZCt7ALT90=/206x305/v2/https://resizing.flixster.com/Fc1FZrej9A-GYxYP3boYzqIQ9Tk=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzRmYzI5Yzc5LWI1NDUtNDk5OC1hNjBkLTRlNGNhZTRjNWE4OS53ZWJw`,
+  }
+}
