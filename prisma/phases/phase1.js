37d1f944bf8111105daa88dd970b2d856bee6480
Umaid Ismail <umaidismail@umaids-air.mynetworksettings.com>
2024-12-03 09:22:47 -0800

    add phase folder

---
+++ prisma/phases/phase1.js
@@ -0,0 +1,12 @@
+const phase1 = {
+  movie: {
+  movieName: "Iron Man",
+  details: "A billionaire industrialist and genius inventor, Tony Stark (Robert Downey Jr.), is conducting weapons tests overseas, but terrorists kidnap him to force him to build a devastating weapon. Instead, he builds an armored suit and upends his captors. Returning to America, Stark refines the suit and uses it to combat crime and terrorism.",
+  starRating: "94% on Rotten Tomatoes",
+  },
+  movie2: {
+    movieName: "The Incredible Hulk",
+    details: ""
+  }
+  
+}
