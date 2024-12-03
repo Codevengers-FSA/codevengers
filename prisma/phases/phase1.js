ab34bc32cacc7fc02d6a085cdb8e722075f0a5c8
Umaid Ismail <umaidismail@umaids-air.mynetworksettings.com>
2024-12-03 10:04:48 -0800

    create phase1

--- prisma/phases/phase1.js
+++ prisma/phases/phase1.js
@@ -1,12 +1,45 @@
 const phase1 = {
   movie: {
-  movieName: "Iron Man",
-  details: "A billionaire industrialist and genius inventor, Tony Stark (Robert Downey Jr.), is conducting weapons tests overseas, but terrorists kidnap him to force him to build a devastating weapon. Instead, he builds an armored suit and upends his captors. Returning to America, Stark refines the suit and uses it to combat crime and terrorism.",
-  starRating: "94% on Rotten Tomatoes",
+  ratings: "93% on Rotten Tomatoes",
+  watchlist: [],
+  title: "Iron Man",
+  summary: "A billionaire industrialist and genius inventor, Tony Stark (Robert Downey Jr.), is conducting weapons tests overseas, but terrorists kidnap him to force him to build a devastating weapon. Instead, he builds an armored suit and upends his captors. Returning to America, Stark refines the suit and uses it to combat crime and terrorism.",
+  image: "https://resizing.flixster.com/3pxO4cKBL9QjNOkV42mTU4USCMc=/206x305/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p170620_p_v13_ae.jpg"
+  
   },
   movie2: {
-    movieName: "The Incredible Hulk",
-    details: ""
-  }
-  
+    ratings: "68% on Rotten Tomatoes",
+    watchlist: [],
+    title: "Incredible Hulk",
+    summary: "Scientist Bruce Banner (Edward Norton) desperately seeks a cure for the gamma radiation that contaminated his cells and turned him into The Hulk. Cut off from his true love Betty Ross (Liv Tyler) and forced to hide from his nemesis, Gen. Thunderbolt Ross (William Hurt), Banner soon comes face-to-face with a new threat: a supremely powerful enemy known as The Abomination (Tim Roth).",
+    image: "https://resizing.flixster.com/vb6Qw2hvo7SxSUqje05l-vvFtEo=/206x305/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p176337_p_v10_ag.jpg"
+  },
+  movie3: {
+    ratings: "72% on Rotten Tomatoes",
+    watchliest: [],
+    title: "Iron Man 2",
+    summary: `With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and "Rhodey" Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.`,
+    image: "https://resizing.flixster.com/y2qtkJifbvzjXPsZfz8NhB2fWU0=/206x305/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p3546118_p_v10_af.jpg"
+  },
+  movie4: {
+    ratings: "77% on Rotten Tomatoes",
+    watchlists: [],
+    title: "Thor",
+    summary: `As the son of Odin (Anthony Hopkins), king of the Norse gods, Thor (Chris Hemsworth) will soon inherit the throne of Asgard from his aging father. However, on the day that he is to be crowned, Thor reacts with brutality when the gods' enemies, the Frost Giants, enter the palace in violation of their treaty. As punishment, Odin banishes Thor to Earth. While Loki (Tom Hiddleston), Thor's brother, plots mischief in Asgard, Thor, now stripped of his powers, faces his greatest threat.`,
+    image:  `https://resizing.flixster.com/z-9rcPQro8MlvlMkSVvLytmc9Fw=/206x305/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p7989358_p_v8_ah.jpg`,
+  },
+  movie5: {
+    ratings: "80% on Rotten Tomatoes",
+    watchlists: [],
+    title: "Captain America: The First Avenger",
+    summary: `It is 1941 and the world is in the throes of war. Steve Rogers (Chris Evans) wants to do his part and join America's armed forces, but the military rejects him because of his small stature. Finally, Steve gets his chance when he is accepted into an experimental program that turns him into a supersoldier called Captain America. Joining forces with Bucky Barnes (Sebastian Stan) and Peggy Carter (Hayley Atwell), Captain America leads the fight against the Nazi-backed HYDRA organization.`,
+    image: `https://resizing.flixster.com/r9nFQAGel-zjc73J1Je2pfypmmM=/206x305/v2/https://resizing.flixster.com/DPxL1sG2snpcMbqGJc67YGTrjlM=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzL2JmZTdkNDA5LTA5NzktNGE3OC04ODE2LTJlOTBkYTA3MDY2NS5qcGc=`,
+  },
+  movie6: {
+    ratings: "91% on Rotten Tomatoes",
+    watchlists: [],
+    title: "Marvel's the Avengers",
+    summary: `When Thor's evil brother, Loki (Tom Hiddleston), gains access to the unlimited power of the energy cube called the Tesseract, Nick Fury (Samuel L. Jackson), director of S.H.I.E.L.D., initiates a superhero recruitment effort to defeat the unprecedented threat to Earth. Joining Fury's "dream team" are Iron Man (Robert Downey Jr.), Captain America (Chris Evans), the Hulk (Mark Ruffalo), Thor (Chris Hemsworth), the Black Widow (Scarlett Johansson) and Hawkeye (Jeremy Renner).`,
+    image: `https://resizing.flixster.com/6WHSHxkCrLKGv6bAXMn1A26ehVM=/206x305/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p8815512_p_v8_ax.jpg`,
+  },
 }
