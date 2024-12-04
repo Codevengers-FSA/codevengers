16e783d0e5e55dd1008126ce5a26f355a30c9d78
kdonovan628 <kelsey.donovan628@gmail.com>
2024-12-04 13:15:26 -0700

    update seed to use upsert over create syntax and deployed database

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,31 +2,33 @@
 const prisma = require("../prisma");
 
 // Import the data for Phase 1 and Phase 2 movies
-const phase1 = require(`./phases/phase1`);
-const phase2 = require(`./phases/phase2`);
-const phase3 = require(`./phases/phase3`);
-const phase4 = require(`./phases/phase4`);
-const phase5 = require(`./phases/phase4`);
+const phase1 = require("./phases/phase1");
+const phase2 = require("./phases/phase2");
+const phase3 = require("./phases/phase3");
+const phase4 = require("./phases/phase4");
+const phase5 = require("./phases/phase5");
 
-
-
-
-// Main seeding function 
-const main =  async () => {
+// Main seeding function
+const main = async () => {
   const movies = [
-      // Combine the movie data from all phases into a single array with spread operator (`...`)
-      ...phase1,
-      ...phase2,
-      ...phase3,
-      ...phase4,
-      ...phase5,
+    // Combine the movie data from all phases into a single array with spread operator (`...`)
+    ...phase1,
+    ...phase2,
+    ...phase3,
+    ...phase4,
+    ...phase5,
   ];
 
-  
   // Iterate over each movie in the array
   for (const movie of movies) {
-    await prisma.movie.create({
-      data: {
+    await prisma.movie.upsert({
+      where: { title: movie.title },
+      update: {
+        ratings: movie.ratings,
+        summary: movie.summary,
+        image: movie.image,
+      },
+      create: {
         title: movie.title,
         ratings: movie.ratings,
         summary: movie.summary,
