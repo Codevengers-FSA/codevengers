45efd4053a8e3699a216955661a572ca66e5207c
kdonovan628 <kelsey.donovan628@gmail.com>
2024-12-06 12:57:17 -0700

    updated phase arrays to have ISO date format, corrected chronological order

--- prisma/phases/phase5.js
+++ prisma/phases/phase5.js
@@ -5,8 +5,8 @@ const phase5 = [
     watchlists: [],
     summary: "Super-Hero partners Scott Lang (Paul Rudd) and Hope van Dyne (Evangeline Lilly) return to continue their adventures as Ant-Man and the Wasp. Together, with Hope's parents Janet van Dyne (Michelle Pfeiffer) and Hank Pym (Michael Douglas), and Scott's daughter Cassie Lang (Kathryn Newton), the family finds themselves exploring the Quantum Realm, interacting with strange new creatures and embarking on an adventure that will push them beyond the limits of what they thought possible.",
     image: "https://resizing.flixster.com/KEZJTcdLgSGr-sY3p5cDYM9HRto=/206x305/v2/https://resizing.flixster.com/_0Mhf0W_vdhwlaqOaUDX6JiReKA=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzE0OGY2MTIxLTE1MDEtNDI5YS1hYzVlLWYyMzExYjBmZTlhMC5qcGc=",
-    releaseDate: "February 17, 2023",
-    chronologicalOrder: 31
+    releaseDate: "2023-02-17",
+    chronologicalOrder: 31,
   },
   {
     ratings: "82% on Rotten Tomatoes",
@@ -14,7 +14,7 @@ const phase5 = [
     watchlists: [],
     summary: `In Marvel Studios "Guardians of the Galaxy Vol. 3" our beloved band of misfits are looking a bit different these days. Peter Quill, still reeling from the loss of Gamora, must rally his team around him to defend the universe along with protecting one of their own. A mission that, if not completed successfully, could quite possibly lead to the end of the Guardians as we know them.`,
     image: "https://resizing.flixster.com/7BvHbqYVikal2I8DAX-HnNSdrdU=/206x305/v2/https://resizing.flixster.com/p8g-u3ZgPuxrg4x7aq1tidyffGg=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzL2FiZDg0OTc0LWVkMTktNDVhYS05ODljLTNkMjYxMjA0OTdjMy5qcGc=",
-    releaseDate: "May 5, 2023",
+    releaseDate: "2023-05-05",
     chronologicalOrder: 32,
   },
   {
@@ -23,7 +23,7 @@ const phase5 = [
     watchlists: [],
     summary: `Carol Danvers aka Captain Marvel has reclaimed her identity from the tyrannical Kree and taken revenge on the Supreme Intelligence. But unintended consequences see Carol shouldering the burden of a destabilized universe. When her duties send her to an anomalous wormhole linked to a Kree revolutionary, her powers become entangled with that of Jersey City super-fan, Kamala Khan aka Ms. Marvel, and Carol's estranged niece, now S.A.B.E.R. astronaut Captain Monica Rambeau. Together, this unlikely trio must team-up and learn to work in concert to save the universe as "The Marvels."`,
     image: "https://resizing.flixster.com/t9xW3hzc3NC_2ICHlzvLe7kvASc=/206x305/v2/https://resizing.flixster.com/Ht14zArBc90eTF1dMAaQynKk2Ic=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzL2NhNjNhOGQ1LWE5Y2YtNDFlZS1iNDk0LWQyZGMyZTlhYmVmOS5qcGc=",
-    releaseDate: "November 11, 2023",
+    releaseDate: "2023-11-11",
     chronologicalOrder: 33,
   },
   {
@@ -32,8 +32,8 @@ const phase5 = [
     watchlists: [],
     summary: "Deadpool's peaceful existence comes crashing down when the Time Variance Authority recruits him to help safeguard the multiverse. He soon unites with his would-be pal, Wolverine, to complete the mission and save his world from an existential threat.",
     image: "https://resizing.flixster.com/OBCMbeYEWS5tI2QhErFskxKyEa0=/206x305/v2/https://resizing.flixster.com/mPJp85eApHd8ih9XF5E9d3-2LbM=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzUxODlkZDE1LTQyYjUtNDg5ZS05NjZmLWMxZDk1YWZhN2E1ZC5qcGc=",
-    releaseDate: "July 26, 2024",
-    chronologicalOrder: 34
+    releaseDate: "2024-07-26",
+    chronologicalOrder: 34,
   },
   {
     ratings: "Not Yet Released",
@@ -41,7 +41,7 @@ const phase5 = [
     watchlists: [],
     summary: "Anthony Mackie returns as the high-flying hero Sam Wilson, who's officially taken up the mantle of Captain America. After meeting with newly elected U.S. President Thaddeus Ross, Sam finds himself in the middle of an international incident. He must discover the reason behind a nefarious global plot before the true mastermind has the entire world seeing red.",
     image: "https://resizing.flixster.com/06rsLR9PCoR7wVbdPpmHoIVprN8=/206x305/v2/https://resizing.flixster.com/qjr40eth3N-Q-H9vMS2K__qYcho=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzQ3NGVhMWU0LTk3NmUtNGI4ZS1hMjhlLWZmMDE0MzU0NTQ3ZS5qcGc=",
-    releaseDate: "February 14, 2025",
+    releaseDate: "2025-02-14",
     chronologicalOrder: 35,
   },
   {
@@ -50,8 +50,8 @@ const phase5 = [
     watchlists: [],
     summary: "A group of supervillains are recruited to go on missions for the government.",
     image: "https://resizing.flixster.com/5-5sJpfzpSaVvxKHb4Mf8O-C1i8=/206x305/v2/https://resizing.flixster.com/AxWKcjpAOlEXEDwlJJ_kDtuMzWU=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzBmZDFiNWM1LTk2NmUtNDQyOS04NGJjLWIwNWU5MmViNjhjOS5qcGc=",
-    releaseDate: "May 5, 2025",
-    chronologicalOrder: 36
+    releaseDate: "2025-05-05",
+    chronologicalOrder: 36,
   },
 ]
 
--- prisma/seed.js
+++ prisma/seed.js
@@ -1,17 +1,13 @@
-// Import the PrismaClient
 const prisma = require("../prisma");
 
-// Import the data for Phase 1 and Phase 2 movies
 const phase1 = require("./phases/phase1");
 const phase2 = require("./phases/phase2");
 const phase3 = require("./phases/phase3");
 const phase4 = require("./phases/phase4");
 const phase5 = require("./phases/phase5");
 
-// Main seeding function
 const main = async () => {
   const movies = [
-    // Combine the movie data from all phases into a single array with spread operator (`...`)
     ...phase1,
     ...phase2,
     ...phase3,
@@ -19,7 +15,6 @@ const main = async () => {
     ...phase5,
   ];
 
-  // Iterate over each movie in the array
   for (const movie of movies) {
     await prisma.movie.upsert({
       where: { title: movie.title },
@@ -27,18 +22,21 @@ const main = async () => {
         ratings: movie.ratings,
         summary: movie.summary,
         image: movie.image,
+        releaseDate: new Date(movie.releaseDate),
+        chronologicalOrder: movie.chronologicalOrder,
       },
       create: {
         title: movie.title,
         ratings: movie.ratings,
         summary: movie.summary,
         image: movie.image,
+        releaseDate: new Date(movie.releaseDate), 
+        chronologicalOrder: movie.chronologicalOrder,
       },
     });
   }
 };
 
-// Call the main function and handle success or failure
 main()
   .then(() => {
     console.log(`Database seeded successfully!`);
