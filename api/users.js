70de3064b5aac7996927f21a22dc3ad6b506e6cf
kdonovan628 <kelsey.donovan628@gmail.com>
2024-12-12 18:45:46 -0700

    added watched movies get and post routes

--- api/users.js
+++ api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { PrismaClient } = require("@prisma/client");
+const { authenticate } = require("../api/auth"); // Import the authenticate middleware
 
 const prisma = new PrismaClient();
 
@@ -24,10 +25,11 @@ router.get("/:id", async (req, res, next) => {
     next(e);
   }
 });
+
 router.get('/:id/comments', async (req, res, next) => {
   const userId = parseInt(req.params.id, 10);
 
-  try{
+  try {
     const userComments = await prisma.comment.findMany({
       where: { userId },
     });
@@ -35,6 +37,45 @@ router.get('/:id/comments', async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-})
+});
+
+// "I've watched this" functionality
+router.post('/:id/watched', authenticate, async (req, res, next) => {
+  const userId = parseInt(req.params.id, 10);
+  const { movieId } = req.body;
+
+  try {
+    const user = await prisma.user.update({
+      where: { id: userId },
+      data: {
+        watchedMovies: {
+          push: movieId,
+        },
+      },
+    });
+
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Get watched movies
+router.get('/:id/watched', authenticate, async (req, res, next) => {
+  const userId = parseInt(req.params.id, 10);
+
+  try {
+    const user = await prisma.user.findUniqueOrThrow({
+      where: { id: userId },
+      select: {
+        watchedMovies: true,
+      },
+    });
+
+    res.status(200).json(user.watchedMovies);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
