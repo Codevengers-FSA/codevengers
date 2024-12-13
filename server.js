0fedc29394db89e165a9f296d0471c37b14a7af3
Kelsey Donovan <kelsey.donovan628@gmail.com>
2024-12-13 15:48:57 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
+app.use(express.static(path.join(__dirname, 'public')));
 const app = express();
 const PORT = process.env.PORT || 3000;
 
